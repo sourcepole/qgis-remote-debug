9b13262ae7080d67435e63a1f5fe46de5d595acd
Horst Duester <horst.duester@sourcepole.ch>
2012-07-30 13:31:04 +0200

    Initial Eric4 implementation

---
+++ RemoteDebug/__init__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ RemoteDebug
+                                 A QGIS plugin
+ Plugin to connect different IDE remote debugger
+                             -------------------
+        begin                : 2012-07-30
+        copyright            : (C) 2012 by Dr. Horst Düster / Pirmin Kalberer /Sourcepole AG
+        email                : horst.duester@sourcepole.ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ This script initializes the plugin, making it known to QGIS.
+"""
+def name():
+    return "Plugin to connect different IDE remote debugger"
+def description():
+    return "Plugin to connect different IDE remote debugger"
+def version():
+    return "Version 0.1"
+def icon():
+    return "icon.png"
+def qgisMinimumVersion():
+    return "1.8"
+def classFactory(iface):
+    # load RemoteDebug class from file RemoteDebug
+    from remotedebug import RemoteDebug
+    return RemoteDebug(iface)
---
+++ RemoteDebug/dbg_client/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2005 - 2011 Detlev Offenbach <detlev@die-offenbachs.de>
+#
+
+"""
+Package implementing the Python debugger
+
+It consists of different kinds of debug clients.
+"""
