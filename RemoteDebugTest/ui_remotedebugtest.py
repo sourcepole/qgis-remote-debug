f314a95e5c488ec03d58f1f5f24b772cf8320e84
Pirmin Kalberer <pka@sourcepole.ch>
2013-07-15 21:20:14 +0200

    Instructions for Eric4

--- RemoteDebugTest/ui_remotedebugtest.py
+++ RemoteDebugTest/ui_remotedebugtest.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui_remotedebugtest.ui'
 #
-# Created: Thu Jul 11 23:50:57 2013
+# Created: Mon Jul 15 21:18:56 2013
 #      by: PyQt4 UI code generator 4.9.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -17,14 +17,14 @@ except AttributeError:
 class Ui_RemoteDebugTest(object):
     def setupUi(self, RemoteDebugTest):
         RemoteDebugTest.setObjectName(_fromUtf8("RemoteDebugTest"))
-        RemoteDebugTest.resize(400, 300)
+        RemoteDebugTest.resize(401, 351)
         self.buttonBox = QtGui.QDialogButtonBox(RemoteDebugTest)
-        self.buttonBox.setGeometry(QtCore.QRect(30, 240, 341, 32))
+        self.buttonBox.setGeometry(QtCore.QRect(40, 300, 341, 32))
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
         self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
         self.buttonBox.setObjectName(_fromUtf8("buttonBox"))
         self.textEdit = QtGui.QTextEdit(RemoteDebugTest)
-        self.textEdit.setGeometry(QtCore.QRect(20, 20, 361, 201))
+        self.textEdit.setGeometry(QtCore.QRect(20, 20, 361, 271))
         self.textEdit.setLineWrapMode(QtGui.QTextEdit.NoWrap)
         self.textEdit.setReadOnly(True)
         self.textEdit.setObjectName(_fromUtf8("textEdit"))
@@ -42,10 +42,18 @@ class Ui_RemoteDebugTest(object):
 "</style></head><body style=\" font-family:\'Ubuntu\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">This dialog throws an exception for testing remote debugging.</span></p>\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Preparation (PyDev):</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Preparation for <span style=\" font-weight:600;\">PyDev</span>:</p>\n"
 "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">- Go to the debug perspective</p>\n"
 "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">- Configure remote debugging of exceptions (PyDev &gt; Manage Exception Breakpoints)</p>\n"
 "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">- Start the debug server (PyDev &gt; Start Debug Server)</p>\n"
 "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">- Press the QGIS toolbar icon to activate remote debugging</p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">- The debugger is activated when a breakpoint is reached or an expception is raised (configureable) </p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">- The debugger is activated when a breakpoint is reached or an expception is raised (configureable)</p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Activate passive debuging with <span style=\" font-weight:600;\">ERIC4</span> are:</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">- open Settings-&gt;Preferences-&gt;Debugging General</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">- activate the option &quot;Passive Debugger Enabled&quot;</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">- Restart the ERIC4 IDE</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">- Load the Plugin-Project</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">- Press the QGIS toolbar icon to activate passive debugging with eric4</p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
 
